53561b047f341b62988bafacb70346b8e0aaf435
Sasho <s.lekoski@scytale.tech>
2022-02-25 21:41:55 +0100

    Dynamic load and test SR

--- src/app/components/auth-guard-pages/auth-guard-pages.component.ts
+++ src/app/components/auth-guard-pages/auth-guard-pages.component.ts
@@ -1,5 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+
+const languages = ['mk', 'sr'];
 
 @Component({
 	selector: 'app-auth-guard-pages',
@@ -10,17 +13,17 @@ export class AuthGuardPagesComponent implements OnInit {
 	constructor(private router: Router) {}
 
 	ngOnInit(): void {
-		let url = this.router.url;
+		const url = this.router.url;
+		const lang = url.slice(1, 3);
 		if (url === '/') {
 			this.router.navigate(['home']);
-		} else if (url.startsWith('/mk') && url.indexOf('#') == -1) {
+			// } else if (url.startsWith('/mk') && url.indexOf('#') == -1) {
+		} else if (languages.indexOf(lang) == -1 && url.indexOf('#') == -1) {
 			console.log('Redirect to:', window.document.location.href);
-			// this.router.navigate([url]);
-			window.document.location.href = window.document.location.origin + '/base/mk/' + '#welcome';
-
-			// window.document.location.href = window.document.location.href + '#1';
+			window.document.location.href =
+				window.document.location.origin + environment.baseFeUrl + url.slice(1, 3) + url.replace('#', '');
 		} else if (url.startsWith('/#')) {
-			this.router.navigate(['welcome']);
+			this.router.navigate([url.replace('#', '')]);
 		}
 	}
 }
