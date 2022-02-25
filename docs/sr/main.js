a25b3f41e72076325a2b529ed806ac52abb304e9
Sasho <s.lekoski@scytale.tech>
2022-02-25 22:09:50 +0100

    fix

--- docs/sr/main.js
+++ docs/sr/main.js
@@ -240,10 +240,10 @@ class AuthGuardPagesComponent {
             this.router.navigate(['home']);
             // } else if (url.startsWith('/mk') && url.indexOf('#') == -1) {
         }
-        else if (languages.indexOf(lang) == -1 && url.indexOf('#') == -1) {
+        else if (languages.indexOf(lang) != -1 && url.indexOf('#') == -1) {
             console.log('Redirect to:', window.document.location.href);
             window.document.location.href =
-                window.document.location.origin + src_environments_environment__WEBPACK_IMPORTED_MODULE_0__.environment.baseFeUrl + url.slice(1, 3) + url.replace('#', '');
+                window.document.location.origin + src_environments_environment__WEBPACK_IMPORTED_MODULE_0__.environment.baseFeUrl + url;
         }
         else if (url.startsWith('/#')) {
             this.router.navigate([url.replace('#', '')]);
--- src/app/components/auth-guard-pages/auth-guard-pages.component.ts
+++ src/app/components/auth-guard-pages/auth-guard-pages.component.ts
@@ -18,10 +18,10 @@ export class AuthGuardPagesComponent implements OnInit {
 		if (url === '/') {
 			this.router.navigate(['home']);
 			// } else if (url.startsWith('/mk') && url.indexOf('#') == -1) {
-		} else if (languages.indexOf(lang) == -1 && url.indexOf('#') == -1) {
+		} else if (languages.indexOf(lang) != -1 && url.indexOf('#') == -1) {
 			console.log('Redirect to:', window.document.location.href);
 			window.document.location.href =
-				window.document.location.origin + environment.baseFeUrl + url.slice(1, 3) + url.replace('#', '');
+				window.document.location.origin + environment.baseFeUrl + url;
 		} else if (url.startsWith('/#')) {
 			this.router.navigate([url.replace('#', '')]);
 		}
